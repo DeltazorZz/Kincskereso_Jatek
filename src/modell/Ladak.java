739037e1884f71f18c30ca2a73c9357eb53fcd8e
DeltazorZz <vetka.adi@gmail.com>
2022-06-06 17:05:05 +0200

    Ládák Osztály létrehozása

---
+++ src/modell/Ladak.java
@@ -0,0 +1,6 @@
+package modell;
+public class Ladak {
+    public static final String KINCS = "Kincs";
+    public static final String NEMTALALT = "Nem talált";
+    public static final String KINCSRELELT ="Kincsre lelt";
+}
